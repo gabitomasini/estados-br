2c25d110a8d1d28c1affa341b60ee378ed04a6e0
gabitomasini <gabriela_tomasini@hotmail.com>
2022-04-30 23:41:55 -0300

    Filtro de estados

--- estados/src/app/app.component.ts
+++ estados/src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'estados';
+  estadosIniciais = [
+    {"nome": "Acre", "sigla": "AC"},
+    {"nome": "Alagoas", "sigla": "AL"},
+    {"nome": "Amapá", "sigla": "AP"},
+    {"nome": "Amazonas", "sigla": "AM"},
+    {"nome": "Bahia", "sigla": "BA"},
+    {"nome": "Ceará", "sigla": "CE"},
+    {"nome": "Distrito Federal", "sigla": "DF"},
+    {"nome": "Espírito Santo", "sigla": "ES"},
+    {"nome": "Goiás", "sigla": "GO"},
+    {"nome": "Maranhão", "sigla": "MA"},
+    {"nome": "Mato Grosso", "sigla": "MT"},
+    {"nome": "Mato Grosso do Sul", "sigla": "MS"},
+    {"nome": "Minas Gerais", "sigla": "MG"},
+    {"nome": "Pará", "sigla": "PA"},
+    {"nome": "Paraíba", "sigla": "PB"},
+    {"nome": "Paraná", "sigla": "PR"},
+    {"nome": "Pernambuco", "sigla": "PE"},
+    {"nome": "Piauí", "sigla": "PI"},
+    {"nome": "Rio de Janeiro", "sigla": "RJ"},
+    {"nome": "Rio Grande do Norte", "sigla": "RN"},
+    {"nome": "Rio Grande do Sul", "sigla": "RS"},
+    {"nome": "Rondônia", "sigla": "RO"},
+    {"nome": "Roraima", "sigla": "RR"},
+    {"nome": "Santa Catarina", "sigla": "SC"},
+    {"nome": "São Paulo", "sigla": "SP"},
+    {"nome": "Sergipe", "sigla": "SE"},
+    {"nome": "Tocantins", "sigla": "TO"}
+
+]
+estados = this.estadosIniciais;
+
+mudaCampo(evento: any){
+  const valor = evento.target.value;
+  let estadosFiltrados: any = [];
+  for(let i = 0; i < this.estadosIniciais.length; i++){
+    if(this.estadosIniciais[i].nome.toLowerCase().normalize('NFD').includes(valor) || this.estadosIniciais[i].sigla.toLowerCase().normalize('NFD').includes(valor)) {
+      this.estados.push(this.estadosIniciais[i]);
+    }
+
+    this.estados = estadosFiltrados;
+  }
+}
+
+
 }
